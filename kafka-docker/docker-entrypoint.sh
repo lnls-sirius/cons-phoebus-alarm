7c38e5b275a92bc5df699880bef475d29998fd0b
carneirofc <claudiofcarneiro@hotmail.com>
2019-11-07 11:42:17 -0300

    docker

---
+++ kafka-docker/docker-entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -x
+IP=$(hostname)
+(cd /opt/kafka &&\
+    sed -i -e 's/dataDir=.*/dataDir=\/opt\/data\/zookeeper/' config/zookeeper.properties)
+(cd /opt/kafka &&\
+    mkdir -p /opt/data/kafka-logs &&\
+    sed -i \
+        -e 's/zookeeper.connection.timeout.ms=.*/zookeeper.connection.timeout.ms=300000/' \
+        -e 's/log\.dirs=.*/log\.dirs=\/opt\/data\/kafka-logs/g' \
+        -e "31s/.*/listeners=PLAINTEXT:\/\/${IP//\./\\\.}:9092/g" \
+        config/server.properties &&\
+    echo "" >> config/server.properties &&\
+    echo "auto.create.topics.enable=false" >> config/server.properties)
+
+# Start zookeeper
+(echo "Starting zookeeper" && cd /opt/kafka; bin/zookeeper-server-start.sh -daemon config/zookeeper.properties)
+wait-for-it -t 0 localhost:2181
+sleep 5
+
+# Start kafka
+(echo "Starting Kafta" && cd /opt/kafka; bin/kafka-server-start.sh -daemon config/server.properties)
+wait-for-it -t 0 ${IP}:9092
+
+(echo "Creating Accelerator topic" && cd /opt/phoebus/app/alarm/examples/ && ln -s /opt/kafka . && ./create_alarm_topics.sh Accelerator)
+
+# Start Alarm Server
+echo "Starting alarm-server for topic Accelerator" &&\
+    cd /opt/phoebus/services/alarm-server && \
+    ./alarm-server.sh -server ${IP}:9092 -config Accelerator -noshell &
+
+# Config Log
+aux=$IP:9092
+mkdir -p /opt/alarm-config-repo &&\
+    echo "Starting alarm-config-logger for topic Accelerator" &&\
+    cd /opt/phoebus/services/alarm-config-logger &&\
+    java -jar target/service-alarm-config-logger-*.jar \
+        -topics Accelerator \
+        -repo.location /opt/alarm-config-repo \
+        -bootstrap.servers $aux &
+
+# Elasticsearch
+cat > '/opt/elasticsearch/config/elasticsearch.yml' << EOF
+transport.host: localhost
+network.host: 0.0.0.0
+http.port: 9200
+EOF
+
+( echo "Starting Elasticsearch" && runuser -m elastic -c 'cd /opt/elasticsearch && ./bin/elasticsearch' )&
+wait-for-it -t 0 ${IP}:9200
+
+( cd /opt/phoebus/services/alarm-logger/startup && sh create_alarm_index.sh accelerator)
+
+tail -f /dev/null
